c2317f33786b58c7ae46c402c17cf0b9e77d7dc1
Michael Tan <michaeltan@MacBook-Pro-2.fios-router.home>
2022-03-26 00:15:06 -0400

    moved about

--- michaelbtan.com/src/components/About/About.js
+++ michaelbtan.com/src/components/About/About.js
@@ -8,16 +8,6 @@ export default function About() {
   return (
     <div className="about">
       <div className="about_container">
-        <div className="about_info">
-          <h1>About Me</h1>
-          <h3>I am software engineer interested in web3 and solving problems using technology</h3>
-          <br />
-          <p>Previously, I had worked as the developer on 2 NFT projects - <b>Onesises World</b> and <b>Kevin Pals</b>, which I had generated the collection of avatars, wrote and deployed an erc721A smart contract, instituted a merkle tree for whitelist validation, and built the minting dapp</p>
-          <br />
-          <p>I am also a collector of limited sneakers, rare basketball/pokemon cards, art, and wine</p>
-          <br />
-          <p><b>Currently, I am open to full-time software engineer roles.</b></p>
-        </div>
         <Carousel className="carousel" showThumbs={false} showIndicators={false} autoPlay={true} infiniteLoop={true} showStatus={false} width="350px">
           <div>
             <img className="about_img" src="https://i.imgur.com/2RDAZGO.jpg" alt="profile pic" />
@@ -35,6 +25,16 @@ export default function About() {
             <img className="about_img" src="https://i.imgur.com/2L4NMMC.png" alt="profile pic" />
           </div>
         </Carousel>
+        <div className="about_info">
+          <h1>About Me</h1>
+          <h3>I am software engineer interested in web3 and solving problems using technology</h3>
+          <br />
+          <p>Previously, I had worked as the developer on 2 NFT projects - <b>Onesises World</b> and <b>Kevin Pals</b>, which I had generated the collection of avatars, wrote and deployed an erc721A smart contract, instituted a merkle tree for whitelist validation, and built the minting dapp</p>
+          <br />
+          <p>I am also a collector of limited sneakers, rare basketball/pokemon cards, art, and wine</p>
+          <br />
+          <p><b>Currently, I am open to full-time software engineer roles.</b></p>
+        </div>
       </div>
     </div>
   )
